e1c2195d0a8b19deaa643c5a6c04c10c03ca602e
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-04-19 11:45:22 +0530

    Add files via upload

---
+++ assignment6.1.cpp
@@ -0,0 +1,16 @@
+//---------------------------------1.PRINT 5*3 STAR PATTERN USINGFOR LOOP--------------------------------------------
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+			cout<<"*"<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
