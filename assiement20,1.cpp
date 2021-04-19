3a85deb3bfd64017d566de157dce1f88a033d18b
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-04-19 11:34:08 +0530

    Add files via upload

---
+++ assiement20,1.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------1.APROGRAM TO FIND AREA OF RECTABGLE(TAKE INPUT FROM USER)----------------------------------------
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	double len,br,area;
+	cout<<"enter the lenght of rectangle:";
+	cin>>len;
+	cout<<"enter the bright of rectangle:";
+	cin>>br;
+	area = len*br;
+	cout<<"area of rectangle is :"<<area;
+	return 0;
+	
+}
