8d0fafcf6a4dea8434cb003481227a8c6d9319ee
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-04-20 11:45:21 +0530

    Add files via upload

---
+++ same_values_rows.cpp
@@ -0,0 +1,16 @@
+//---------------------------same value in rows -----------------------------
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			cout<<i<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
