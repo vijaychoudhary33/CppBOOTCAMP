ff519f12fff305dff63020605ee705d29200501b
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-04-20 20:22:28 +0530

    Add files via upload

---
+++ triangle_using_forloop_pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int s=5;s>=i;s--)
+		{
+			cout<<" ";
+		}
+		for(int j=0;j<i;j++)
+		{
+			cout<<"* ";
+			
+			
+		}
+			cout<<"\n";
+		}
+		
+	
+	return 0;
+}
