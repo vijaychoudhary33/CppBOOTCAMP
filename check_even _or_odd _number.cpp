5c72a70c49efb682a2fa801ee9d8232575ab60e6
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-04-30 22:01:48 +0530

    check number is odd or even

---
+++ check_even _or_odd _number.cpp
@@ -0,0 +1,19 @@
+//------------------------------------1.A PROGRAM TO ACCEPT A NUMBER AND CHECK WETHER IT IS ODD OR EVEN------------------------------------------------------------
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a;
+	 cout<<"ENTER THE NUMBER :";
+	 cin>>a;
+	 if(a%2==0)
+	 {
+	 	cout<<"YOUR ENTERED NUMBER IS EVEN.";
+	 	
+	 }
+	 else
+	 {
+	 	cout<<"YOUR ENTERD NUMBER IS ODD.";
+	 }
+	 return 0;	
+}
