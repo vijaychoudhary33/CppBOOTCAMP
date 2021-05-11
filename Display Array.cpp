685b80957abba1abfb443a8073f2eb134cf24542
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-05-11 20:54:28 +0530

    Display array entered by user

---
+++ Display Array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,a[n];
+   cout<<"enter the how many elements in  :";
+   cin>>n;
+   for( int i=0;i<n;i++)
+   {
+   	cout<<"enter the "<< i <<"element";
+   	cin>>a[i];
+   }
+      for( int i=0;i<n;i++)
+   {
+   	 cout<<a[i];
+   }
+   
+   return 0;
+
+
+   	
+}
