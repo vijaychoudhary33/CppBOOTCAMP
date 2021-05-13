64049a50feed3539c5a33ef35ba3beedd0e173d3
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-05-13 21:01:11 +0530

    print using the switch statement

---
+++ Switch statement.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char x;
+	cout<<"enter";
+	cin>>x;
+	switch(x)
+	{
+		case "A":
+			cout<<"bad";
+			break;
+		case "B":
+		     cout<<"good";
+			 break; 	
+	}
+	return 0;
+	
+}
