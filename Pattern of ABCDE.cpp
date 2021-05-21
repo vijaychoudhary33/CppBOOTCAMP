aee25ecea7f2bbf5640e3dbddef7a4c3d5096d1e
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-05-21 23:03:37 +0530

    show the patter of ABCDE

---
+++ Pattern of ABCDE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char i,j;
+	for(i='E';i>='A';i--)
+	{
+		for(j='A';j<=i;j++)
+		{
+			cout<<i<<"  ";
+			
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
