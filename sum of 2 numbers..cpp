0e6476cdf6c7a355ed593285ae986cdafe7e38c4
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-07-05 21:22:51 +0530

    sum of two numbers

---
+++ sum of 2 numbers..cpp
@@ -0,0 +1,14 @@
+//sum of 2 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2,sum=0;
+	cout<<"enter first integer number:";
+	cin>>n1;
+	cout<<"enter second integer number:";
+	cin>>n2;
+	sum = n1+n2;
+	cout<<n1<<"+"<<n2<<"="<<sum;
+	return 0;
+}
