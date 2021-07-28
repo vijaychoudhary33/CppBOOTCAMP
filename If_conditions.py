7a604250a6f9e5f807e2788e84c920b11f319012
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-07-28 22:21:02 +0530

    If conditions

---
+++ If_conditions.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number")) 
+b=int(input("enter the number"))
+c=(input("enter the symbol +,-"))
+if c='+':
+   print(a+b) 
+elif c='-':
+   print(a-b) 
+elif c='*':
+   print(a*b) 
+else:
+   print(a/b) 
+
